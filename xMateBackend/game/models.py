7ba0e4159d9d1ea3a702421dc334286614a6f119
manish sharma dev <manishvsharma1@gmail.com>
2025-04-12 17:13:11 -1200

    created the leaderboard model

--- xMateBackend/game/models.py
+++ xMateBackend/game/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.utils.timezone import now
 
 # Create your models here.
@@ -10,10 +10,10 @@ class Game(models.Model):
         ('completed','Completed'),
     ]
 
-    player_1 = models.ForeignKey(User,related_name='game_as_player1',on_delete=models.CASCADE)
-    player_2 = models.ForeignKey(User,related_name='game_as_player2', on_delete=models.CASCADE)
+    player_1 = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='game_as_player1',on_delete=models.CASCADE)
+    player_2 = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='game_as_player2', on_delete=models.CASCADE)
     moves = models.JSONField(default=list)
-    winner = models.ForeignKey(User,related_name="game_won",null=True,blank=True, on_delete=models.SET_NULL)
+    winner = models.ForeignKey(settings.AUTH_USER_MODEL,related_name="game_won",null=True,blank=True, on_delete=models.SET_NULL)
     status = models.CharField(max_length=20,choices=STATUS_CHOICE,default='pending')
     created_at = models.DateTimeField(now)
     updated_at = models.DateTimeField(auto_now=True)
