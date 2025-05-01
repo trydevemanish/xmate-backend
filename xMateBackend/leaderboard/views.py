887634c73a0b038d46b2e2d3a784082d76c79085
manish sharma dev <manishvsharma1@gmail.com>
2025-04-30 17:44:36 -1200

    leaderboard working fine

--- xMateBackend/leaderboard/views.py
+++ xMateBackend/leaderboard/views.py
@@ -1,16 +1,27 @@
 from django.http import JsonResponse
-from models import Leaderboard
+from . models import Leaderboard
 
 # Create your views here.
 def fetchleaderBoardResultForAll(request):
-    if request.method == 'POST':
+    if request.method == 'GET':
         try:
-            leaderboardresult = Leaderboard.objects.get()
+            leaderboardresult = Leaderboard.objects.all()
 
-            if not leaderboardresult or not leaderboardresult.length == 0:
+            if not leaderboardresult:
                 return JsonResponse({'message':'No leaderboard result fetched'},status=400)
             
-            return JsonResponse({'message':'fetched leaderboard result'},status=200)
+            serialiseddata = [
+                {
+                    'id' : item.user.id,
+                    'name' : item.user.username,
+                    'gameplayed' : item.user.total_game_played,
+                    'rank' : item.rank,
+                    'totalpoints' : item.points
+                }
+                for item in leaderboardresult
+            ]
+            
+            return JsonResponse({'message':'fetched leaderboard result','data':serialiseddata},status=200)
 
         except Exception as e:
             return JsonResponse({'message':f'Failed to Fetch leaderboard result : {str(e)}'},status=500)
