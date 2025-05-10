de334a9da5beb8b11e765881fb777d9b43c9b92e
manish sharma dev <manishvsharma1@gmail.com>
2025-05-09 17:46:38 -1200

    Made some changes to game database

--- xMateBackend/game/models.py
+++ xMateBackend/game/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.timezone import now
+import uuid
 
 # Create your models here.
 class Game(models.Model):
@@ -9,10 +10,16 @@ class Game(models.Model):
         ('in_progress','In_Progess'),
         ('completed','Completed'),
     ]
+    PLAYER_2_JOINING_STATUS = [
+        ('player_2_not_joined','Player_2_Not_Joined'),
+        ('player_2_joined','Player_2_Joined')
+    ]
+    game_id = models.UUIDField(default=uuid.uuid4,editable=False,unique=True,name="game_id")
     player_1 = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='game_as_player1',null=True,on_delete=models.CASCADE)
     player_2 = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='game_as_player2',null=True,on_delete=models.CASCADE)
     moves = models.JSONField(default=list)
     winner = models.ForeignKey(settings.AUTH_USER_MODEL,related_name="game_won",null=True,blank=True, on_delete=models.SET_NULL)
+    player_2_status = models.CharField(max_length=30,choices=PLAYER_2_JOINING_STATUS,default='player_2_not_joined')
     status = models.CharField(max_length=20,choices=STATUS_CHOICE,default='pending')
     created_at = models.DateTimeField(now)
     updated_at = models.DateTimeField(auto_now=True)
--- xMateBackend/game/views.py
+++ xMateBackend/game/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from utils.protectedroute import protectedRoute
+from . models import Game
 
 # Create your views here.
 @csrf_exempt
@@ -16,7 +17,20 @@ def createMatchbtwChallengers(request):
             if not player1userid:
                 return JsonResponse({'message':'Un authorised user'},status=400)
             
-             
+            matchCreated = Game.objects.create(
+                player_1 = player1userid,
+            )
+
+            if not matchCreated:
+                return JsonResponse({'message':'failed to create match'},status=400)
+            
+            return JsonResponse({
+                'message':'Match Created',
+                'game_id' : matchCreated.game_id,
+                'player_1' : matchCreated.player_1,
+                'player_2' : matchCreated.player_2,
+                'status' : matchCreated.status
+            },status=201)
 
         except Exception as e:
             return JsonResponse({'message':f'Failed to create match btw challengerss: {str(e)}'},status=500)
