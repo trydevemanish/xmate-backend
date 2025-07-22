a4aef55622538730777487e1709802e1b5a16b14
manish sharma dev <manishvsharma1@gmail.com>
2025-07-22 20:54:33 +0530

    Created some controller for game match

--- xMateBackend/game/views.py
+++ xMateBackend/game/views.py
@@ -5,10 +5,11 @@ from utils.protectedroute import protectedRoute
 from . models import Game
 from .serializers import GameSerializer
 from rest_framework import status
+from django.db.models import Q
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-# Create your views here.
+# Create your views here. 
 @csrf_exempt
 @protectedRoute
 def createMatchbtwChallengers(request):
@@ -196,9 +197,93 @@ def finding_user_pending_or_inprogess_games(request):
     else:
         return JsonResponse({'message':'Invalid request'},status=status.HTTP_400_BAD_REQUEST)
 
+# finding two games either user created or joined a macth created by another game 
+@csrf_exempt
+@protectedRoute
+def finding_user_recent_two_games(request):
+    if request.method == 'GET':
+        try:
+            userid = request.userid
 
+            if not userid:
+                return JsonResponse({'message':'UnAuthorised user'},status=status.HTTP_400_BAD_REQUEST)
+            
+            try:
+                user_instance = User.objects.get(id=userid)
+                if not user_instance or user_instance is None:
+                    return JsonResponse({'message':'user id is not valid'},status=status.HTTP_400_BAD_REQUEST)
+            except:
+                return JsonResponse({'message': 'User not found'},status=status.HTTP_404_NOT_FOUND)
+            
+            two_recent_game_played_by_user = Game.objects.filter(
+                Q(player_1=user_instance) | Q(player_2=user_instance)
+            ).order_by('-created_at')[:2]
+
+            if not two_recent_game_played_by_user:
+                return JsonResponse({'message':'User have not joined any game'},status=status.HTTP_404_NOT_FOUND)
+            
+            two_recent_game_played_by_user_serialised_data = GameSerializer(two_recent_game_played_by_user)
+
+            if not two_recent_game_played_by_user_serialised_data:
+                return JsonResponse({'message':'Issue Occured while serialising game data'},status=status.HTTP_400_BAD_REQUEST)
+            
+            return JsonResponse({'message':'found two recent game','data':two_recent_game_played_by_user_serialised_data.data},status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return JsonResponse({'message':f'Failed to find user pending game match: {str(e)}'},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    else:
+        return JsonResponse({'message':'Invalid request'},status=status.HTTP_400_BAD_REQUEST)
+
+# deleting a game would lead to decrese the ranking and other detail of the user like total game played ect .. only when game status is completed
+@csrf_exempt
+@protectedRoute
+def delete_a_game(request):
+    if request.method == 'DELETE':
+        try:
+            userid = request.userid
+
+            if not userid:
+                return JsonResponse({'message':'UnAuthorised user'},status=status.HTTP_400_BAD_REQUEST)
+            
+            try:
+                user_instance = User.objects.get(id=userid)
+                if not user_instance or user_instance is None:
+                    return JsonResponse({'message':'user id is not valid'},status=status.HTTP_400_BAD_REQUEST)
+            except User.DoesNotExist:
+                return JsonResponse({'message': 'User not found'},status=status.HTTP_404_NOT_FOUND)
+            
+            data = json.loads(request.body)
+            game_id = data.get('game_id')
+
+            if not game_id:
+                return JsonResponse({'message':'game_id is needed'},status=status.HTTP_400_BAD_REQUEST)
+            
+            checkifgameidexits = Game.objects.filter(game_id=game_id).exists()
+
+            if not checkifgameidexits:
+                return JsonResponse({'message':'Invalid game_id'},status=status.HTTP_400_BAD_REQUEST)
+            
+            try:
+                game_instance = Game.objects.get(game_id=game_id)
+            except Game.DoesNotExist:
+                return JsonResponse({'message': 'Game not found'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            # steps need to do 
+            if game_instance.player_1 != user_instance:
+                return JsonResponse({'message':'you cannot delete the game only the match creater can delete'},status=status.HTTP_400_BAD_REQUEST)
+            
+            game_instance.delete()
+
+            return JsonResponse({'message':'Game deleted'},status=status.HTTP_204_NO_CONTENT)
+            # if game.status is completed than if game is deleted than only we need to make changes to the user_instance 
+            
+
+        except Exception as e:
+            return JsonResponse({'message':f'Failed to find user pending game match: {str(e)}'},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    else:
+        return JsonResponse({'message':'Invalid request'},status=status.HTTP_400_BAD_REQUEST)
 
 # needs to handle these routes 
-# fetch recent 2 games of login user
-# fetch games of the login user that are pending or in progress
+# fetch recent 2 games of login user - done
+# fetch games of the login user that are pending or in progress - done
 # if the game is not completed but you still need to create a new game due to any issue like player 2 is not avialabel or not want to complete the match etc in that case you can probably delete that game match and create a new game
