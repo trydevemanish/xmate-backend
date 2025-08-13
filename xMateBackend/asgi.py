caec290836b92343a356a31af5ccb71455ff0b5e
manish sharma dev <manishvsharma1@gmail.com>
2025-08-13 19:10:40 +0530

    Fix ASGI early import

--- xMateBackend/asgi.py
+++ xMateBackend/asgi.py
@@ -16,10 +16,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 # application = get_asgi_application()
 
 import os
-from pathlib import Path
 import sys
+import django
+from pathlib import Path
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xMateBackend.settings')
+django.setup() 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(BASE_DIR))
@@ -27,7 +31,6 @@ sys.path.append(str(BASE_DIR))
 from game.routing import websocket_urlpatterns
 from django.core.asgi import get_asgi_application
 django_asgi_app = get_asgi_application()
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xMateBackend.settings')
 
 
 application = ProtocolTypeRouter(
